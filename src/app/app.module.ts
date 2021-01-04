4fafd3b85966b98760a9655b23ca49439c5a89d9
TicoYk <didimoykym@gmail.com>
2021-01-04 13:56:54 -0300

    adicionado roteamento e modulo de tarefas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
+import { TarefasModule } from './tarefas/tarefas.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
