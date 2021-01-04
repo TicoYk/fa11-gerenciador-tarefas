c2adbef916dc09c8525639b975dd59500dbec205
TicoYk <didimoykym@gmail.com>
2021-01-04 13:56:05 -0300

    alterado titulo da aplicacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fa11-gerenciador-tarefas';
+  title = 'FA-11 Gerenciador de Tarefas';
 }
