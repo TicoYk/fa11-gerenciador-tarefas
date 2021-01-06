3f166f5c3a13a85f23c25c3abf8a15725e08fd49
TicoYk <didimoykym@gmail.com>
2021-01-05 23:41:26 -0300

    adicionados modulos de rota e formulario

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,12 +1,19 @@
 import { TarefaService } from './shared/tarefa.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListagemComponent } from './listagem/listagem.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EditarComponent } from './editar/editar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListagemComponent, CadastrarComponent, EditarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TarefaService
